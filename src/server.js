68983fab016c116e7e358f488b2765e0704e2b86
Elen Salustiano <elen.salustiano@dextra-sw.com.br>
2021-03-21 17:34:01 -0300

    feat: add mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 
 const { ApolloServer } = require('apollo-server')
 const { loadFiles } = require('graphql-import-files')
+const mongoose = require('mongoose');
 
 const resolvers = require('./resolvers')
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.im8i7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+)
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
