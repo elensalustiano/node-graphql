2f5d55f11f1a95764e10091ba07064de4a052a38
Elen Salustiano <elen.salustiano@dextra-sw.com.br>
2021-04-23 21:31:50 -0300

    feat(context): add validation to token type and user data

--- src/auth/context.js
+++ src/auth/context.js
@@ -4,17 +4,21 @@ const { dencrypt } = require('./auth')
 const userRepository = require('../repositories/user')
 
 module.exports = async ({ req }) => {
-  const token = req.headers.authorization
-  if (!token) return {}
+  let user
 
-  const { data: { id } } = dencrypt(token)
-  console.log('id: ', id)
-  const user = await userRepository.findById(id)
+  if (req && req.headers && req.headers.authorization) {
+    const [type, token] = req.headers.authorization.split(' ')
+
+    if (type === 'Bearer' && token) {
+      const { data: { id } } = dencrypt(token)
+      user = await userRepository.findById(id)
+    }
+  }
 
   return {
     user,
     validateUser() {
-      throw AuthenticationError('Access denied, you must be authenticated')
+      if (!user) throw new AuthenticationError('Access denied, you must be authenticated')
     }
   }
 }
